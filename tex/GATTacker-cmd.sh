31b5d2b24d8678a5f11eb6d6aab6c38e822db0d9
ShurayukiHime <jos.7-11@hotmail.it>
2018-07-24 03:02:00 +0200

    proofread ch3 fixed appendix a,b

---
+++ tex/GATTacker-cmd.sh
@@ -0,0 +1,5 @@
+root@kali:~/node_modules/gattacker# node scan.js
+
+root@kali:~/node_modules/gattacker# node scan c6f8ce66737e
+
+root@kali:~/node_modules/gattacker# node advertise.js -a devices/c6f8ce66737e_GAPButt-.adv.json -s devices/c6f8ce66737e.srv.json 
---
+++ tex/bluetoothctl-board.sh
@@ -0,0 +1,4 @@
+connect C6:F8:CE:66:73:7E
+list-attributes
+select-attribute /org/bluez/hci0/dev_C6_F8_CE_66_73_7E/service000c
+read
